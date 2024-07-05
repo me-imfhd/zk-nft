9e2fa21b88e9c69ff9d228ce47251f968ed39e80
sol-idity <168968506+sol-idity@users.noreply.github.com>
2024-07-06 06:41:29 +0800

    first commit

---
+++ anchor/programs/zk-nft/src/errors.rs
@@ -0,0 +1,15 @@
+use anchor_lang::prelude::*;
+
+#[error_code]
+pub enum ZkNftError {
+    #[msg("GroupMaxSizeExceeded")]
+    GroupMaxSizeExceeded,
+    #[msg("GroupAuthorityOrDelegateMismatch")]
+    GroupAuthorityOrDelegateMismatch,
+    #[msg("AssetNotMutable")]
+    AssetNotMutable,
+    #[msg("AssetIsLocked")]
+    AssetIsLocked,
+    #[msg("Authority is not the owner or delegate")]
+    InvalidAuthority,
+}
---
+++ anchor/programs/zk-nft/src/state/group.rs
@@ -0,0 +1,10 @@
+use anchor_lang::prelude::*;
+
+#[account]
+#[derive(InitSpace)]
+pub struct Group {
+    pub size: u64,
+    pub max_size: u64,
+    pub authority: Pubkey,
+    pub delegate: Option<Pubkey>,
+}
